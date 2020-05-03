5c5dffc634cc51e3f8a23c8b4a00e0dc4eb0f78b
John Doe <sakshikshatriya99@gmail.com>
2020-05-03 14:13:24 +0530

    line chart

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends React.Component{
                 <Cards data={data}/>
                 <CountryPicker  />
                 <Chart />
+                
             </div>
         );
     };
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,49 +1,52 @@
 import React, { useState , useEffect} from 'react' ;
 import {Line, Bar } from 'react-chartjs-2';
 import styles from './Chart.module.css';
+import {fetchDailyData} from '../../api';
+
 
 const Chart = () => {
 
+    const [dailyData, setDailyData ]=useState([]);
+    useEffect(()=>{
+      const fetchAPI=async()=>{
+        console.log(dailyData);
+        setDailyData(await fetchDailyData());
+      }
+
+      console.log(dailyData);
+      fetchAPI();
+    });
+
     const lineChart =(
-        <Line 
+      dailyData.length 
+        ?(<Line 
                data={ 
                         {
-                            labels :["January", "February","March", "April" ,"May "] ,
+                            labels : dailyData.map(({date})=> date),
                             datasets : [
                             {
-                           
-                            data: [
-                              100,
-                              250,
-                              322,
-                              350,
-                              500,
-                            ],
-                            label : 'Confirmed',
-                            fill: true,
-                            borderColor: '#3333ff'
-                             } , {
-                               
-                                data: [
-                                  20,
-                                  110,
-                                  190,
-                                  250,
-                                  300,
-                                ],
-                                label : 'Deaths',
-                                fill: true,
-                                borderColor: 'red',
-                                backgroundColor: 'rgba(255,0,0,0.5)'
-                                 }
-                           ]        
+                              data:dailyData.map(({confirmed})=> confirmed),
+                              label : 'Infected',
+                              borderColor:'#3333ff',
+                              fill: true
+                            },
+                            
+                           {
+                            data:dailyData.map(({deaths})=> deaths),
+                            label : 'Deaths',
+                            borderColor:'red',
+                            backgroundColor: 'rgba(255, 0 ,0 ,0.5)',
+                            
+                            fill: true
+                            } 
+                           ]       
                }
             }         
-        />
+        />) :null
 
     );
 
-    const barChart = (
+   /* const barChart = (
         <Bar>
           data = {{
               labels: ['Infected', 'Recovered', 'Deaths'],
@@ -61,7 +64,7 @@ const Chart = () => {
             title : {display : true , text : 'Current state '},
           }}
         </Bar>
-    );
+    );*/
     return(
     <div className={styles.container}>
         {lineChart}
